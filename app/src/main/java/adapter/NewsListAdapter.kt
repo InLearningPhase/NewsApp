ed62931dee80353837436695a95f48f6c8bebf9b
InLearningPhase <taneshgupta2211@gmail.com>
2020-11-09 14:55:02 +0530

    add images and author to show in the news

--- app/src/main/java/adapter/NewsListAdapter.kt
+++ app/src/main/java/adapter/NewsListAdapter.kt
@@ -3,8 +3,10 @@ package adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.newsapp.News
 import com.example.newsapp.R
 
@@ -30,6 +32,8 @@ class NewsListAdapter( private val listener: NewsItemClicked) : RecyclerView.Ada
 
         val currentItem = items[position]
         holder.titleView.text = currentItem.title
+        holder.author.text = currentItem.author
+        Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.imageView)
 
     }
 
@@ -44,6 +48,8 @@ class NewsListAdapter( private val listener: NewsItemClicked) : RecyclerView.Ada
 class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val titleView: TextView = itemView.findViewById(R.id.title)
+    val imageView: ImageView = itemView.findViewById(R.id.image)
+    val author: TextView = itemView.findViewById(R.id.author)
 
 }
 
